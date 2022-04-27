1b3eb41437a99942c32294ca3d40b349ae47d6a1
Maciej Uberna <maciej.uberna@gmail.com>
2022-04-27 20:56:06 +0200

    App.js added router animated transision animations between pages.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import Layout from './hoc/Layout/Layout';
 import O_MNIE from './coponents-stateLess/Content/AboutMe/Info';
 import Galerie from './coponents-stateLess/Content/FotoGalley/Gallery';
@@ -8,19 +9,23 @@ import Galerie from './coponents-stateLess/Content/FotoGalley/Gallery';
 import './App.css';
 
 function App() {
+  const location = useLocation();
+
   return (
     <Layout>
-      <Suspense fallback={<p>Loading ...</p>}>{routes}</Suspense>
+      <Suspense fallback={<p>Loading ...</p>}>
+        <TransitionGroup component={null}>
+          <CSSTransition key={location.key} classNames="linkAnimation" timeout={300}>
+            <Routes>
+              <Route path="/photo-gallery/info" element={<O_MNIE />}/> 
+              <Route path='/photo-gallery/photos' element={<Galerie />}/>
+              <Route path="/" element={<O_MNIE />} />
+            </Routes>
+          </CSSTransition>
+        </TransitionGroup>
+      </Suspense>
     </Layout>
   );
 }
-
-let routes = (
-  <Routes>
-    <Route path="/photo-gallery/info" element={<O_MNIE />}/> 
-    <Route path='/photo-gallery/photos' element={<Galerie />}/>
-    <Route path="/" element={<O_MNIE />} />
-  </Routes>
-);
 
 export default App;
